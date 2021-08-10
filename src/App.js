d75186c0aab3d0585ba2c3bdb58c3c5622ed9320
Rachel Bates <rachelbatesnj@gmail.com>
2021-08-10 12:25:43 -0400

    adding states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import MainBlock from './components/MainBlock'
 import './App.css';
+import { useState } from 'React';
 
 function App() {
+  const [moodChoice, setMoodChoice] = useState(null)
+  const [view, setView] = useState('home')
+
   return (
     <>
       <h1>Let's get a read on your mood today!</h1>
